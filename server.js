b7b707bc47f7af286e8795dde2375af8bf8606a3
amr080 <117864716+amr080@users.noreply.github.com>
2025-06-20 20:14:18 -0400

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,9 @@ const fs = require('fs');
 const path = require('path');
 const { exec } = require('child_process');
 const app = express();
-
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
 app.use(express.static('.'));
 
 const excludedRootItems = [
